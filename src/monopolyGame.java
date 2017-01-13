423b2c61ea3c007d469ab51a49946fa278bbe239
miroslavitaly <miroslav.italy@gmail.com>
2017-01-13 19:16:40 +0200

    add buying, who wins and other

--- src/monopolyGame.java
+++ src/monopolyGame.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 public class monopolyGame {
 
 	public static void main(String[] args) {
-		
-		//How many players
 		Scanner sc1 = new Scanner(System.in);
-		System.out.println("How many players");
-		int howManyPlayers = sc1.nextInt(4);
+		System.out.println("How many players will play from 1 to 4 ?");
+		int howManyPlayers = sc1.nextInt(5);
 
-		// Enter player name
 		String[] playerName = new String[howManyPlayers];
 		for (int i = 0; i <= playerName.length - 1; i++) {
 			Scanner sc2 = new Scanner(System.in);
-			System.out.println("Enter player name" + i + " :");
+			System.out.println("Enter the name of the player " + (i + 1) + " :");
 			String name = sc2.next();
 			playerName[i] = name;
 		}
@@ -23,157 +20,140 @@ public class monopolyGame {
 		for (int i = 0; i < player.length; i++) {
 			player[i] = i;
 		}
-		//Player account
+		// запазва сметката на играча
 		int[] sum = new int[howManyPlayers];
 		for (int i = 0; i < sum.length; i++) {
 			sum[i] = 1000;
 		}
-		//Position Player
+		// запазва позицията на играча
 		int[] positionPlayer = new int[howManyPlayers];
-		//Property player
+		// запазва собствеността на играча
 		int[] property = new int[5];
 		for (int i = 0; i < property.length; i++) {
-			property[i] = 9;
+			property[i] = 6;
 		}
-		//Position name
-		String[] positionName = { " Стартово поле ", " Съкровище ", " Ботевградско шосе: 50$ ",
-				" Цариградско шосе: 60$ ", " Данък печалба: 200$ " };
-		int [] positionPrise = {10, 10, 50, 60, 200};
+		// имената на позициите
+		String[] positionName = { " START ", " 1- Special place: GOLD", " 2- ", " 3- ", " 4- PAY TAX 200 $", " 5- ",
+				" 6- Special place: CARD", " 7- ", " 8- ", " 9- ", " 10- ", " 11- ", " 12- ", " 13- ", " 14- ", " 15- ",
+				" 16- ", " 17- ", " 18- ", " 19- ", " 20- ", " 21- ", " 22- ", " 23- ", " 24- ", " 25- ", " 26- ",
+				" 27- ", " 28- ", " 29- ", " 30- ", " 31- ", " 32- ", " 33- ", " 34- ", " 35- ", " 36- ", " 37- ",
+				" 38- ", " 39- ", };
+		int[] positionPrise = { 100, 200, 300, 400, 600 };
 
 		boolean runGame = true;
-
-		//Start game
+		int playersInGame = howManyPlayers;
+		int winner = 0;
+		// Старт на ирата
 		while (runGame) {
 			for (int j = 0; j <= player.length - 1; j++) {
-
-				System.out.println(playerName[j] + " , твой ред е! Натисни /Enter/! ");
+				if (sum[j] <= 0) {
+					continue;
+				}
+				System.out.println(playerName[j] + " , its your turn! Press enter!");
 				Scanner sc3 = new Scanner(System.in);
 				char run = sc3.next(".").charAt(0);
 
-				System.out.println("По твоята сметка има: " + sum[j] + " долара!");
-
 				Random rand = new Random();
 				int dice = rand.nextInt(4) + 1;
-				System.out.println("Сума от заровете: " + dice);
+				System.out.println("The sum of the dice: " + dice);
 
-				System.out.println("Предходна позиция " + positionPlayer[j] + positionName[positionPlayer[j]]);
+				System.out.println("Previous   position:" + positionName[positionPlayer[j]]);
 				positionPlayer[j] = passStart(positionPlayer[j], dice);
-				System.out.println("Настояща  позиция " + positionPlayer[j] + positionName[positionPlayer[j]]);
-				System.out.println("Ти си върху " + positionName[positionPlayer[j]] + "Собственост на "
-						+ Arrays.toString(property));
-
-				// " Стартово поле "
-				if (positionPlayer[j] == 0) {
-
-					System.out.println("Ти си върху /Начало/");
-
+				System.out.println("Current    position:" + positionName[positionPlayer[j]]);
+				if (property[positionPlayer[j]] == 6) {
+					System.out.println("Position is free to buy! Prise :  " + positionPrise[positionPlayer[j]] + " dollars!");
+				} else {
+					System.out.println("Position is property to: "
+							+ playerName[property[positionPlayer[j]]]);
 				}
-				// " Съкровище "
-				if (positionPlayer[j] == 1) {
-					System.out.println(playerName[j] + " ,ти би могъл да купиш имота! Купуваш ли? "
-							+ " y - за Да ;  n - за Не");
-					Scanner sc4 = new Scanner(System.in);
+				System.out.println("On your account has: " + sum[j] + " dollars!");
+
+				//if (positionPlayer[j] == positionPlayer[j]) {
+					if ((property[positionPlayer[j]] == 6) && (sum[j] >= positionPrise[positionPlayer[j]])) {
+						System.out.println(playerName[j] + " Do you want to buy? " + " y - for Yes ,or  n - for No");
+						Scanner sc4 = new Scanner(System.in);
 						char bayOrNo = sc4.next(".").charAt(0);
 						switch (bayOrNo) {
-						case 'y':property[positionPlayer[j]] = bay(property[positionPlayer[j]], player[j]);
-								System.out.println("Ти купи: " + positionName[positionPlayer[j]]);
-								sum[j] -= positionPrise[positionPlayer[j]]; 
+						case 'y':
+							property[positionPlayer[j]] = buy(property[positionPlayer[j]], player[j]);
+							System.out.println("You bought: " + positionName[positionPlayer[j]]);
+							sum[j] -= positionPrise[positionPlayer[j]];
 							break;
 						case 'n':
-							break;
+							continue;
 						default:
-							break;
+							System.out.println("Wrong index!");
+							continue;
 						}
-					sum[j] = payRent(property[positionPlayer[j]], player[j], sum[j]);
-					sum[property[positionPlayer[j]]] = getMoney(property[positionPlayer[j]], player[j], 
-							sum[property[positionPlayer[j]]]);
-
-				}
-				// " Ботевградско шосе: 50$ "
-				if (positionPlayer[j] == 2) {
-					System.out.println(playerName[j] + " ,ти би могъл да купиш имота! Купуваш ли? "
-							+ " y - за Да ;  n - за Не");
-					Scanner sc4 = new Scanner(System.in);
-						char bayOrNo = sc4.next(".").charAt(0);
-						switch (bayOrNo) {
-						case 'y':property[positionPlayer[j]] = bay(property[positionPlayer[j]], player[j]);
-								System.out.println("Ти купи: " + positionName[positionPlayer[j]]);
-								sum[j] -= positionPrise[positionPlayer[j]];
-							break;
-						case 'n':
-							break;
-						default:
-							break;
-						}
-					sum[j] = payRent(property[positionPlayer[j]], player[j], sum[j]);
-					sum[property[positionPlayer[j]]] = getMoney(property[positionPlayer[j]], player[j], 
-							sum[property[positionPlayer[j]]]);
-
-				}
-				// " Цариградско шосе: 60$ "
-				if (positionPlayer[j] == 3) {
-					System.out.println(playerName[j] + " ,ти би могъл да купиш имота! Купуваш ли? "
-							+ " y - за Да ;  n - за Не");
-					Scanner sc4 = new Scanner(System.in);
-						char bayOrNo = sc4.next(".").charAt(0);
-						switch (bayOrNo) {
-						case 'y':property[positionPlayer[j]] = bay(property[positionPlayer[j]], player[j]);
-						System.out.println("Ти купи: " + positionName[positionPlayer[j]]);
-						sum[j] -= positionPrise[positionPlayer[j]];
-							break;
-						case 'n':
-							break;
-						default:
-							break;
-						}
-					sum[j] = payRent(property[positionPlayer[j]], player[j], sum[j]);
-					sum[property[positionPlayer[j]]] = getMoney(property[positionPlayer[j]], player[j], 
-							sum[property[positionPlayer[j]]]);
-
-				}
-				// " Данък печалба: 200$ "
-				if (positionPlayer[j] == 4) {
-					
-					//за оправяне, защото разрешава постоянно купуване
-					System.out.println(playerName[j] + " ,ти би могъл да купиш имота! Купуваш ли? "
-							+ " y - за Да ;  n - за Не");
-					Scanner sc4 = new Scanner(System.in);
-						char bayOrNo = sc4.next(".").charAt(0);
-						switch (bayOrNo) {
-						case 'y':property[positionPlayer[j]] = bay(property[positionPlayer[j]], player[j]);
-						System.out.println("Ти купи: " + positionName[positionPlayer[j]]);
-						sum[j] -= positionPrise[positionPlayer[j]];
-							break;
-						case 'n':
-							break;
-						default:
-							break;
-						}
-					sum[j] = payRent(property[positionPlayer[j]], player[j], sum[j]);
-					sum[property[positionPlayer[j]]] = getMoney(property[positionPlayer[j]], player[j], 
-							sum[property[positionPlayer[j]]]);
-
-				}
-				System.out.println("По твоята сметка остават: " + sum[j] + " долара!");
-				System.out.println("Сметка на играчите " + Arrays.toString(sum));
-				//if (sum[j] == 0) {
-				//	System.out.println(player[j] + "Ти излизаш от играта");
-				//	runGame = false;
+					} else if ((property[positionPlayer[j]] == 6) && (sum[j] < positionPrise[positionPlayer[j]])) {
+						System.out.println("You do not have enough money to buy!");
+						continue;
+					}
+					if (sum[property[positionPlayer[j]]] > 0) {
+						sum[j] = payRent(property[positionPlayer[j]], player[j], sum[j],
+								positionPrise[positionPlayer[j]], playerName[property[positionPlayer[j]]]);
+						sum[property[positionPlayer[j]]] = getMoney(property[positionPlayer[j]], player[j],
+								sum[property[positionPlayer[j]]], positionPrise[positionPlayer[j]]);
+					}
+					winner = isWinner(sum[j], player[j]);
+					System.out.println("Accounts of players " + Arrays.toString(sum));
+					System.out.println("------------------------------------------------");
 				//}
+				for (int i = 0; i < sum.length; i++) {
+					if (sum[i] <= 0) {
+						for (int k = 0; k < property.length; k++) {
+							for (int k2 = 0; k2 < property.length; k2++) {
+								if (property[k2] == player[i]){
+									System.out.println("You have : " + Arrays.toString(positionName));
+								}
+							}
+							if (property[k] == player[i]) {
+								System.out.println(playerName[i] + " ,do you want to sell yuor property? "
+										+ " y - for YES ;  n - for NO");
+								Scanner sc5 = new Scanner(System.in);
+								char sellOrNo = sc5.next(".").charAt(0);
+								switch (sellOrNo) {
+								case 'y':
 
+									Scanner sc6 = new Scanner(System.in);
+									System.out.println("Select position number to sell:");
+									int whatToSell = sc6.nextInt();
+									if (property[whatToSell] == player[i]) {
+										property[whatToSell] = 6;
+										sum[i] += (positionPrise[whatToSell] * 0.5);
+										System.out.println("Now in your account has: " + sum[i] + " dollars.");
+									}
+									break;
+								case 'n':
+									continue;
+								default:
+									System.out.println("Wrong index!");
+									continue;
+								}
+
+							}
+						}
+						
+					}
+
+				}
+
+				System.out.println("Position property " + Arrays.toString(property));
+				System.out.println("Accounts of players " + Arrays.toString(sum));
+				if (sum[j] <= 0) {
+					System.out.println(playerName[player[j]] + "! Game over for you!");
+					playersInGame--;
+				}
+				
+				System.out.println("------------------------------------------------");
 			}
-
+			if (playersInGame <= 1) {
+				runGame = false;
+				System.out.println("Game over! " + playerName[player[winner]] + ", you win!");
+			}
 		}
-
 	}
-
 	public static int passStart(int positionPlayer, int dice) {
-		// positionPlayer[j] = positionPlayer[j] + dice;
-		// if (positionPlayer[j] > 4) {
-		// positionPlayer[j] -= 5;
-		// } else if (positionPlayer[j] == 5) {
-		// positionPlayer[j] = 0;
-		// }
 		positionPlayer = positionPlayer + dice;
 		if (positionPlayer > 4) {
 			positionPlayer -= 5;
@@ -182,33 +162,34 @@ public class monopolyGame {
 		}
 		return positionPlayer;
 	}
-	public static int bay(int propertyPositionPlayerJ, int playerJ){
-		// Bay
-		if (propertyPositionPlayerJ == 9) {
-			propertyPositionPlayerJ = playerJ;
-		}
+	public static int buy(int propertyPositionPlayerJ, int playerJ) {
+		propertyPositionPlayerJ = playerJ;
 		return propertyPositionPlayerJ;
 	}
-	public static int payRent(int propertyPositionPlayerJ, int playerJ, int sumJ){
+	public static int payRent(int propertyPositionPlayerJ, int playerJ, int sumJ, int positionPrisePpositionPlayerJ,
+			String playerNamePropertyPositionPlayerJ) {
 		// Pay rent
 		if (propertyPositionPlayerJ != playerJ) {
-					if(sumJ>0){
-						sumJ -= 200;
-					}
+			sumJ -= (positionPrisePpositionPlayerJ * 0.5);
+			System.out.println("You pay " + (positionPrisePpositionPlayerJ * 0.5) + " dollars rent to "
+					+ playerNamePropertyPositionPlayerJ);
 		}
 		return sumJ;
 	}
-	public static int getMoney(int propertyPositionPlayerJ, int playerJ, int sumPropertyPositionPlayerJ){
+	public static int getMoney(int propertyPositionPlayerJ, int playerJ, int sumPropertyPositionPlayerJ,
+			int positionPrisePpositionPlayerJ) {
 		if (propertyPositionPlayerJ != playerJ) {
-						sumPropertyPositionPlayerJ += 200;
+			sumPropertyPositionPlayerJ += (positionPrisePpositionPlayerJ * 0.5);
 		}
-						return sumPropertyPositionPlayerJ;
+		return sumPropertyPositionPlayerJ;
 	}
-	public static int sell(int n){
-		// Sell
-		return n;
+	public static int isWinner(int sumJ, int playerJ) {
+		int previousMaxAmount = 0;
+		int playerMaxSum = 0;
+		if (sumJ > previousMaxAmount) {
+			previousMaxAmount = sumJ;
+			playerMaxSum = playerJ;
+		}
+		return playerMaxSum;
 	}
-}	
-	
-
-
+}
